00f3d60e4db54f54c24bf311ddb51cdeeeb9f114
ayan4m1 <andrew@bulletlogic.com>
2011-07-24 15:11:39 -0400

    Added rudimentary build script

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+CLASSPATH=./lib/craftbukkit-0.0.1-SNAPSHOT.jar
+
+rm -rf bin
+mkdir bin
+javac -classpath $CLASSPATH -d bin src/com/ayan4m1/multiarrow/*.java src/com/ayan4m1/multiarrow/arrows/*.java
+
+cd bin
+cp ../plugin.yml .
+jar cf ../MultiArrow.jar ./*
