014a3befe8deb7d6fd6d9902f41415dc5e89e946
ayan4m1 <andrew@bulletlogic.com>
2011-07-25 14:50:27 -0400

    Made Drill arrow more powerful

--- src/com/ayan4m1/multiarrow/arrows/DrillArrowEffect.java
+++ src/com/ayan4m1/multiarrow/arrows/DrillArrowEffect.java
@@ -1,34 +1,40 @@
 package com.ayan4m1.multiarrow.arrows;
 
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
-import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Entity;
+import org.bukkit.util.Vector;
 
 public class DrillArrowEffect implements CustomArrowEffect {
 
 	@Override
 	public void hitEntity(Arrow arrow, Entity target) {
-		this.drillDown(target.getWorld().getBlockAt(target.getLocation()), 1);
+		this.drillDown(target.getWorld().getBlockAt(target.getLocation()));
 		arrow.remove();
 	}
 
 	@Override
 	public void hitGround(Arrow arrow) {
-		this.drillDown(arrow.getWorld().getBlockAt(arrow.getLocation()), 2);
+		this.drillDown(arrow.getWorld().getBlockAt(arrow.getLocation()));
 		arrow.remove();
 	}
 	
-	private void drillDown(Block startBlock, int numBlocks) {
-		Block entityBlock = startBlock;
-		for(int i = 0; i < 2; i++) {
+	private void drillDown(Block startBlock) {
+		for(int y = 0; y >= -2; y--) {
 			//Don't increase the count for snow
-			if (entityBlock.getType() == Material.SNOW) {
-				i--;
+			if (startBlock.getType() == Material.SNOW) {
+				y++;
 			}
-			entityBlock.setType(Material.AIR);
-			entityBlock = entityBlock.getRelative(BlockFace.DOWN);
+			for (int x = -1; x < 1; x++) {
+				for (int z = -1; z < 1; z++) {
+					Vector offset = startBlock.getLocation().toVector().add(new Vector(x, y, z));
+					Block locBlock = startBlock.getWorld().getBlockAt(new Location(startBlock.getWorld(), offset.getX(), offset.getY(), offset.getZ()));
+
+					locBlock.setType(Material.AIR);
+				}
+			}
 		}
 	}
 
