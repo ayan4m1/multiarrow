a1b1174cec23e6fcff5fe9ae6ff4aa63644126e3
ayan4m1 <andrew@bulletlogic.com>
2011-07-25 15:22:13 -0400

    Cleaned up BlockHitDetector and added timeout to save memory

--- src/com/ayan4m1/multiarrow/MultiArrowBlockHitDetector.java
+++ src/com/ayan4m1/multiarrow/MultiArrowBlockHitDetector.java
@@ -8,32 +8,45 @@ import org.bukkit.Location;
 public class MultiArrowBlockHitDetector implements Runnable {
 	private MultiArrow plugin;
 	private HashMap<Arrow, Location> locations;
-	
+	private HashMap<Arrow, Integer> counts;
+
 	public MultiArrowBlockHitDetector(MultiArrow instance) {
 		plugin = instance;
 		locations = new HashMap<Arrow, Location>();
+		counts = new HashMap<Arrow, Integer>();
 	}
-	
+
 	@Override
 	public void run() {
 		if (plugin.activeArrowEffect.size() > 0) {
 			Object[] activeArrows = plugin.activeArrowEffect.keySet().toArray();
-			for(int i = 0; i < activeArrows.length; i++) {
-				Arrow arrow = (Arrow)activeArrows[i];
-	            if(locations.containsKey(arrow)) {
-	                Location loc = (Location)locations.get(arrow);
-	                Location loca = arrow.getLocation();
-	                if (loc.getBlockX() == loca.getBlockX() && loc.getBlockY() == loca.getBlockY() && loc.getBlockZ() == loca.getBlockZ()) {
-	                    plugin.activeArrowEffect.get(arrow).hitGround(arrow);
-	                    plugin.activeArrowEffect.remove(arrow);
-	                    locations.remove(arrow);
-	                } else {
-	                    locations.put(arrow, arrow.getLocation());
-	                }
-	            } else {
-	                locations.put(arrow, arrow.getLocation());
-	            }
-	        }
+			for (int i = 0; i < activeArrows.length; i++) {
+				Arrow arrow = (Arrow) activeArrows[i];
+				if (locations.containsKey(arrow) && counts.containsKey(arrow)) {
+					if (counts.get(arrow) > 100) {
+						arrow.remove();
+						plugin.activeArrowEffect.remove(arrow);
+						locations.remove(arrow);
+						counts.remove(arrow);
+						continue;
+					}
+
+					Location loc = (Location) locations.get(arrow);
+					Location loca = arrow.getLocation();
+					if (loc.getBlockX() == loca.getBlockX() && loc.getBlockY() == loca.getBlockY() && loc.getBlockZ() == loca.getBlockZ()) {
+						plugin.activeArrowEffect.get(arrow).hitGround(arrow);
+						plugin.activeArrowEffect.remove(arrow);
+						locations.remove(arrow);
+						counts.remove(arrow);
+					} else {
+						locations.put(arrow, arrow.getLocation());
+						counts.put(arrow, counts.get(arrow) + 1);
+					}
+				} else {
+					locations.put(arrow, arrow.getLocation());
+					counts.put(arrow, 0);
+				}
+			}
 		}
 	}
 }
